44281e725b007d7cf21c2ed013476c4ec8f07e9e
Deniss <cvetkovsdeniss4@gmail.com>
2025-10-24 13:15:28 +0300

    Added and configured for work new app

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,10 @@ use App\Http\Controllers\ProductController;
 
 Route::redirect('/', '/products');
 
-Route::get('/products', [ProductController::class, 'index'])->name('products/index');
-Route::get('/products/create', [ProductController::class, 'create'])->name('products/create');
-Route::post('/products', [ProductController::class, 'store'])->name('products/store');
-Route::get('/products/{products}/show', [ProductController::class, 'show'])->name('products/show');
-Route::delete('/products/{products}', [ProductController::class, 'destroy'])->name('products/destroy');
-Route::get('/products/{products}/edit', [ProductController::class, 'edit'])->name('products/edit');
-Route::put('/products/{products}', [ProductController::class, 'update'])->name('products/update');
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+Route::get('/products/{products}/show', [ProductController::class, 'show'])->name('products.show');
+Route::delete('/products/{products}', [ProductController::class, 'destroy'])->name('products.destroy');
+Route::get('/products/{products}/edit', [ProductController::class, 'edit'])->name('products.edit');
+Route::put('/products/{products}', [ProductController::class, 'update'])->name('products.update');
