4be4bc8ce36f697aada41cd7f22ed89a5d21fc00
Deniss <cvetkovsdeniss4@gmail.com>
2025-10-24 13:22:49 +0300

    Improved navigation

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,6 +1,6 @@
 <nav>
     <ol>
-        <li><a href="">Index</a></li>
-        <li><a href="">Create</a></li>
+        <li><a href="{{ route('products.index') }}">Index</a></li>
+        <li><a href="{{ route('products.create') }}">Create</a></li>
     </ol>
 </nav>
